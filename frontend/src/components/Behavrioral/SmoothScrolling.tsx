e31a8d7ee85eb284243f45084fed0ed3b995aea5
mohamed-ndoye811 <ndoyemohamed.d@gmail.com>
2024-06-11 14:03:34 +0200

    fix smooth scroll error

--- frontend/src/components/Behavrioral/SmoothScrolling.tsx
+++ frontend/src/components/Behavrioral/SmoothScrolling.tsx
@@ -5,7 +5,7 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }: React.PropsWithChildren): React.ReactElement {
     return (
-        <ReactLenis root options={{ lerp: 0.08, duration: 1.5, smoothTouch: true }}>
+        <ReactLenis root options={{ lerp: 0.08, duration: 1.5 }}>
             {children}
         </ReactLenis>
     );
